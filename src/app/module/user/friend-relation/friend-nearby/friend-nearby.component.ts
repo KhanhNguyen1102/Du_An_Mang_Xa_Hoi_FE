3ed8320d2c19e8ed3e5ee87e542e86295a937557
DuyLeVu <levuduy132@gmail.com>
2022-02-10 12:02:49 +0700

    coding giao diện kết bạn

---
+++ src/app/module/user/friend-relation/friend-nearby/friend-nearby.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-friend-nearby',
+  templateUrl: './friend-nearby.component.html',
+  styleUrls: ['./friend-nearby.component.css']
+})
+export class FriendNearbyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/module/user/user.module.ts
+++ src/app/module/user/user.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { UserRoutingModule } from './user-routing.module';
+import { FriendNearbyComponent } from './friend-relation/friend-nearby/friend-nearby.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    FriendNearbyComponent
+  ],
   imports: [
     CommonModule,
     UserRoutingModule
